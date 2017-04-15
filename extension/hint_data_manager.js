0729db6f27fffd62ea19449daa223decb5556fb6
LiuYue <hangxingliu@gmail.com>
2017-04-15 20:30:20 +0800

    modification in main codes

--- extension/hint_data_manager.js
+++ extension/hint_data_manager.js
@@ -14,29 +14,42 @@ function removeUglyCharactersInCompletionItem(text = '') {
 		.replace(/“`(.+?)`”/g, '“$1”');
 }
 
+let ZERO = ['00000', '0000', '000', '00', '0', ''];
+function getSortPrefix(index) {
+	let indexStr = String(index);
+	return ZERO[indexStr.length] + indexStr;
+}
+
 function initialize() {
 	directivesCompletionItems = [];
 	varCompletionItems = [];
 	directivesItems = require(DIRECTIVES_FILE);
 	varItems = require(VARIABLES_FILE);
-	directivesItems.forEach(directive => {
-		// hide default core directive module name
-		directive.module == 'Core functionality' &&
-			(directive.module = '');	
+
+	directivesItems.forEach((directive, index) => {
+		let isCoreFunc = directive.module == 'Core functionality';
 
 		let item = new vscode.CompletionItem(directive.name,
 			vscode.CompletionItemKind.Property);
-		item.documentation = directive.syntax + '\n' + directive.desc;
-		item.detail = directive.module;
+		item.documentation = directive.syntax.join('\n') + '\n' + directive.desc;
+
+		// hide default core directive module name
+		item.detail = isCoreFunc ? '' : directive.module;
 
 		item.insertText = new vscode.SnippetString(
 			directive.def  // has default value
 				? directive.def.replace(/^(\w+)(\s+)(.+);$/, '$1$2$${params:$3};') 
-				: `${directive.name} \$\{0\};`);
-
+				: `${directive.name}\$\{0\};`);
+		
+		item.sortText = getSortPrefix(index) + item.label;
+		
+		//for fuzzy matching
 		item.filter = directive.name.split('_');
+		//for checking parent block name
+		item.contexts = directive.contexts || [];
 		directivesCompletionItems.push(item);
 	});
+
 	varItems.forEach(v => {
 		let name =  v.name.slice(v.name.startsWith('$')?1:0)
 		let item = new vscode.CompletionItem(
@@ -48,32 +61,46 @@ function initialize() {
 	});
 }
 
-function getVariableCompletionItems(wordNo$) {
+function getVariableCompletionItems(varNamePrefix) {
 	return varCompletionItems.filter(v =>
-		v.label.startsWith(wordNo$) ||
-		v.filter.reduce((macthed, keyword) => macthed || keyword.startsWith(wordNo$), false));
+		v.label.startsWith(varNamePrefix) ||
+		v.filter.reduce((macthed, keyword) => macthed || keyword.startsWith(varNamePrefix), false));
 }
 
-function getDirectiveCompletionItems(wordNo$) {
-	return directivesCompletionItems.filter(v =>
-		v.label.startsWith(wordNo$) ||
-		v.filter.reduce((macthed, keyword) => macthed || keyword.startsWith(wordNo$), false));
+function getDirectiveCompletionItems(directiveNamePrefix, parentBlockName) {
+	//Empty prefix , return all items
+	if (!directiveNamePrefix)
+		return directivesCompletionItems;
+	return directivesCompletionItems.filter(it => {
+		//If there has a specific parent block
+		if (parentBlockName)	
+			if (it.contexts.indexOf(parentBlockName) == -1 && it.contexts[0] != 'any')
+				return false;
+		
+		if (it.label.startsWith(directiveNamePrefix)) return true;
+
+		//fuzzy matching
+		for (let name of it.filter)
+			if (name.startsWith(directiveNamePrefix))
+				return true;	
+	});
 }
 
-function getVariableItem(variableNameWith$) {
+function getVariableItem(variableName) {
+	variableName = `$${variableName}`;
 	for (var index in varItems)
-		if (varItems[index].name == variableNameWith$)
+		if (varItems[index].name == variableName)
 			return varItems[index];
 }
 
-function getDirectiveItem(directiveNameWith$) {
+function getDirectiveItem(directiveName) {
 	for (var index in directivesItems)
-		if (directivesItems[index].name == directiveNameWith$)
+		if (directivesItems[index].name == directiveName)
 			return directivesItems[index];
 }
 
-function genDirectiveHoverHintItem(directive) {
-	let it = directive;
+function genDirectiveHoverHintItem(directiveItem) {
+	let it = directiveItem;
 	if (!it) return null;
 
 	let hover = [
@@ -86,20 +113,18 @@ function genDirectiveHoverHintItem(directive) {
 	return new vscode.Hover(hover.concat(it.notes));
 }
 
-function genVariableHoverHintItem(variable) {
-	let it = variable;
+function genVariableHoverHintItem(variableItem) {
+	let it = variableItem;
 	return it ? new vscode.Hover([`**${it.name}** ${it.module}`, it.desc]) : null;
 }
 
-function genDirectiveParametersHintItem(directive) {
-	let it = directive;
+function genDirectiveParametersHintItem(directiveItem) {
+	let it = directiveItem;
 	if (!it) return null;
 	let sh = new vscode.SignatureHelp();
 	sh.activeSignature = 0;
-	sh.signatures = it.syntax
-		.replace(/;\s*$/, '')
-		.split(';').map(syntax =>
-			new vscode.SignatureInformation(syntax + ';', it.desc));
+	sh.signatures = it.syntax.map(syntax =>
+		new vscode.SignatureInformation(syntax, it.desc));
 	return sh;
 }
 
--- extension/main.js
+++ extension/main.js
@@ -1,71 +1,106 @@
 /// <reference path="../vscode.d.ts" />
 let vscode = require('vscode'),
-	helper = require('./vscode_helper'),
-	hint = require('./hint_data_manager');
+	nginxDocument = require('./open_nginx_document'),
+	hint = require('./hint_data_manager'),
+	{ getTextAroundCursor, getTextBeforeCursor } = require('./vscode_helper');
 
-let DOCUMENT_SELECTOR = ['NGINX'];
+let NGINX_LANGUAGE_ID = 'NGINX';
+let DOCUMENT_SELECTOR = [NGINX_LANGUAGE_ID];
+
+// ==============================
+//      Semantization function 
+//            语义化函数
+// ==============================
+let isCursorInTheDirectiveName = textBeforeCursor => textBeforeCursor.match(/^\s+(\w*)$/);
+let getVariableNamePrefixBeforeCursor = textBeforeCursor => (textBeforeCursor.match(/\$(\w+)$/) || [])[1];
+let couldNotMatchVariableName = variableNamePrefix => typeof variableNamePrefix == 'undefined';
+let showErrorMessage = msg => vscode.window.showErrorMessage(`nginx-conf-hint: ${msg}`);
+
+/**
+ * get which configuration block cursor located in.
+ * @returns {string|undefined}
+ */
+function getBlockNameCursorIn(document, position){
+	let lineNum = position.line, stack = 0, match, line = '', commentMark;
+	while (--lineNum >= 0) {
+		line = document.lineAt(lineNum).text;
+		
+		// ignore comments
+		commentMark = line.indexOf('#');
+		if (commentMark >= 0) line = line.slice(0, commentMark);
+
+		line = line.trim();
+
+		if (line == '}') stack++;
+		
+		if (match = line.match(/^(\w+)\s+.*\{$/)) {
+			if(!stack)
+				return match[1];
+			stack--;
+		}
+	}
+};
+
 
 function activate(context) {
-	let subscriptions = context.subscriptions,
-		sub1, sub2, sub3;
-	
+	console.log('nginx-conf-hint activating...');
+
+	let subscriptions = context.subscriptions, sub1, sub2, sub3, sub4;
+
 	hint.initialize();
+	nginxDocument.initialize(context);
 
 	//======================================
 	//          Completion Item	
 	//======================================	
 	sub1 = vscode.languages.registerCompletionItemProvider(DOCUMENT_SELECTOR, {
-		provideCompletionItems: (document, position /*, token*/ ) => {
-			let beforeText = helper.getTextBeforeCursor(document, position);
-			let directiveMatch = beforeText.match(/^\s+(\w*)$/);
-			if (directiveMatch) {
-				//directive
-				directiveMatch = directiveMatch[1];
-				if (!directiveMatch) return hint.getAllDirectivesItems();
-				return hint.getDirectiveCompletionItems(directiveMatch);
-			}
-			beforeText = (
-				beforeText.match(/\$(\w+)$/) ||
-				beforeText.match(/(\d{1,3})$/) || [])[1];
-			// console.log(beforeText);
-			if (!beforeText) return null;
-			if (beforeText == '$') return hint.getAllVarCompletionItems();
-			let items = hint.getVariableCompletionItems(beforeText.toLowerCase());
-			return items;
+		provideCompletionItems: (document, position) => {
+			// console.log(getBlockNameCursorIn(document, position));
+			let beforeText = getTextBeforeCursor(document, position);
+			
+			let isDirective = isCursorInTheDirectiveName(beforeText),
+				directiveNamePrefix = isDirective && isDirective[1];
+			if (isDirective)
+				return hint.getDirectiveCompletionItems(directiveNamePrefix,
+					getBlockNameCursorIn(document, position));
+			
+			let variableNamePrefix = getVariableNamePrefixBeforeCursor(beforeText);
+			if (couldNotMatchVariableName(variableNamePrefix))
+				return null;	
+			return hint.getVariableCompletionItems(variableNamePrefix);
 		},
-		resolveCompletionItem: (item /*, token*/ ) => item
+		resolveCompletionItem: item => item
 	}, '$');
 
-
+	
 
 	//======================================
 	//          Hover	
 	//======================================
 	sub2 = vscode.languages.registerHoverProvider(DOCUMENT_SELECTOR, {
-		provideHover: (document, position /*, token*/) => {
+		provideHover: (document, position) => {
 			
-			let beforeText = helper.getTextBeforeCursor(document, position);
+			let beforeText = getTextBeforeCursor(document, position);
 
-			let name = helper.getTextAroundCursor(document, position);
+			let name = getTextAroundCursor(document, position);
 			if (!name) return null;
 
 			if (beforeText.match(/^\s*(\w+)$/))
 				return hint.genDirectiveHoverHintItem(hint.getDirectiveItem(name));
 			
-			name = name.match(/^\d{3}$/) ? name : `$${name}`;
 			return hint.genVariableHoverHintItem(hint.getVariableItem(name));
 		}
 	});
 
-
+	
 
 	//======================================
 	//          Parameters Hint	
 	//======================================	
 	sub3 = vscode.languages.registerSignatureHelpProvider(DOCUMENT_SELECTOR, {
-		provideSignatureHelp: (document, position /*, token*/) => {
+		provideSignatureHelp: (document, position) => {
 			
-			let beforeText = helper.getTextBeforeCursor(document, position);
+			let beforeText = getTextBeforeCursor(document, position);
 			let directiveName = beforeText.match(/^\s*(\w+)\s*/);
 
 			return directiveName ?
@@ -74,10 +109,40 @@ function activate(context) {
 		}
 	}, ' ');
 
+	//======================================
+	//          Open document command	
+	//======================================
+	sub4 = vscode.commands.registerCommand('nginx-conf-hint.showDocument', () => {
 
+		let editor = vscode.window.activeTextEditor;
+		if (!editor || editor.document.languageId != NGINX_LANGUAGE_ID)
+			return showErrorMessage('Have not opened a nginx configuration document!');
+
+		let { document, selection } = editor;
+		let text = (selection.isEmpty
+			? getTextAroundCursor(document, selection.start)
+			: document.getText(selection)).trim();
+
+		console.log("request open nginx document: " + text);
+
+		if (!text)
+			return showErrorMessage(`There not any word around cursor or selected!`);	
+		
+		if (nginxDocument.containsThisDirective(text))
+			return nginxDocument.openDirectiveDoc(text.trim());
+		if (nginxDocument.containsThisVariable(text))
+			return nginxDocument.openVariableDoc(text.trim());
+		
+		return showErrorMessage(`"${text}" is not a nginx directive or nginx embedded variables!`);	
+	});	
+	
+	
 	subscriptions.push(sub1);
 	subscriptions.push(sub2);
 	subscriptions.push(sub3);
+	subscriptions.push(sub4);
+	
+	console.log('nginx-conf-hint activated');
 }
 
 function deactivate() { }
