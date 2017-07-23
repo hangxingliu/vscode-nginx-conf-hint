941f70083863481391f11f11f510d365795f0b1f
LiuYue <hangxingliu@gmail.com>
2017-07-24 05:42:22 +0800

    modify snippet source filesystem structure. add access_log to server snippet

--- utils/lib/snippet_generator.js
+++ utils/lib/snippet_generator.js
@@ -1,30 +1,20 @@
-/**
- * Special body for some block with parameters. such as location, upstream...
- */
-let specialSnippetBody = {
-	location: 'location ${location:/} {\n\t$0\n}',
-	upstream: 'upstream ${upstream_name} {\n\t$0\n}'
-};
+//@ts-check
 
-let extraSnippet = {
-	"Block server with directives": {
-		prefix: 'server',
-		body: [
-			'server {',
-			'\tlisten ${address:80};',
-			'\tserver_name ${server_names:server_names};',
-			'\t$0',
-			'}'
-		]
-	}
-};
+let fs = require('fs'),
+	{ ok } = require('./checker');
+require('colors');
+
+const SNIPPETS_DIR = `${__dirname}/../../snippets_src/`;
+
+let specialSnippetBody = require('../../snippets_src/_block_with_parameter'),
+	extraSnippets = loadExtraSnippets();
 
 let generateBlockSnippetObject = blockName => ({
 	prefix: blockName,
 	body: specialSnippetBody[blockName] || `${blockName} {\n\t$0\n}`
 });
 /**
- * @param {Array<object>} directives 
+ * @param {Array<any>} directives 
  */
 function generate(directives) {
 	let contextsMap = {},
@@ -39,9 +29,22 @@ function generate(directives) {
 	Object.keys(contextsMap).forEach(blockName =>
 		result[`Block ${blockName}`] = generateBlockSnippetObject(blockName));
 	
-	Object.keys(extraSnippet).forEach(name => result[name] = extraSnippet[name]);
+	Object.keys(extraSnippets).forEach(name => result[name] = extraSnippets[name]);
 
 	return result;
+}
+
+function loadExtraSnippets() {
+	console.log('Scanning snippet files ...');
+	let snippetFileNames = fs.readdirSync(SNIPPETS_DIR)
+		.filter(name => !name.startsWith('_') && name.endsWith('.js'));
+	let extraSnippets = {};
+	ok(); console.log('Total snippet files:', String(snippetFileNames.length).bold );
+	snippetFileNames.map(fname => require(SNIPPETS_DIR + fname))
+		.forEach(snippets => 
+			Object.keys(snippets).forEach(name => extraSnippets[name] = snippets[name]));
+	console.log('Total snippets:', String(Object.keys(extraSnippets).length).bold);
+	return extraSnippets;
 }
 
 module.exports = {
